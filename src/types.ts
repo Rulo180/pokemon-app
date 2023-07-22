ca6943cf51f06ee049a0588bb63d9dd8d0be79f7
Martin Valles <mvallesacc@gmail.com>
2023-07-22 13:49:30 -0300

    Initial state

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export type PokemonName = {
+  english: string;
+  japanese: string;
+  chinese: string;
+  french: string;
+};
+
+export type PokemonStats = {
+  HP: string;
+  Attack: string;
+  Defense: string;
+  'Sp. Attack': string;
+  'Sp. Defense': string;
+  Speed: string;
+};
+
+export type Pokemon = {
+  name: PokemonName;
+  type: string[];
+  stats: PokemonStats;
+};
+
+export type SerializedPokemon = Omit<Pokemon, 'name'> & {
+  image: string;
+  name: PokemonName['english'];
+};
